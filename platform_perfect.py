69dfcb682b25c1bac3ff96e3515d2f9526d36994
black-lotus713 <104050348+black-lotus713@users.noreply.github.com>
2022-05-06 21:31:12 -0700

    Update platform_perfect.py
    applied feedback to replace lists with dicts and remove loops to find

--- platform_perfect.py
+++ platform_perfect.py
@@ -2,22 +2,25 @@ import ffmpeg
 import os
 import sys
 
+#vestigial cli args
+#platform = sys.argv[1]
+#media_file = sys.argv[2]
+
 path = os.getcwd()
-
-
-##list of codec:libraries
-codeclist_file = path + os.path.sep + 'clist.txt'
-
-codeclist_list = []
-
-with open(codeclist_file, 'r') as c:
-    for line in c.readlines():
-        codeclist_list.append(line.strip())
-
 
 ##list must be platform:get_video_codec
 ##e.g.  "youtube:vp9"
 formatlist_file = path + os.path.sep + 'flist.txt'
+
+##list of codec:libraries
+codeclist_file = path + os.path.sep + 'clist.txt'
+
+codec_dict = {}
+with open(codeclist_file, 'r') as c:
+    for line in c.readlines():
+        codec_item = line.strip()
+        key,value = codec_item.split(':',1)
+        codec_dict[key] = value
 
 def get_video_codecs(media):
     streams = ffmpeg.probe(media)["streams"]
@@ -26,57 +29,41 @@ def get_video_codecs(media):
     for stream in streams:
         if stream["codec_type"] == "video":
             vid_codecs.append(stream["codec_name"])
-
     return vid_codecs
 
 def get_library(format):
-    for codec in codeclist_list:
-        if format in codec:
-            c_ix = codec.index(':')
-            library = codec[c_ix + 1:]
-            return library
-    return 'missing'
+    if format in codec_dict:
+        library = codec_dict[format]
+        return library
+    else:
+        return None
 
 def pp_ffmpeg(platform, media_file):
+    format_dict = {}
+    try:
+        with open(formatlist_file, 'r') as f:
+            for line in f.readlines():
+                format_item = line.strip()
+                key,value = format_item.split(':',1)
+                format_dict[key] = value
 
-    formatlist_list = []
-
-    with open(formatlist_file, 'r') as f:
-        for line in f.readlines():
-            formatlist_list.append(line.strip())
-
-    platformatlist_list = []
-
-    for line in formatlist_list:
-        if platform in line:
-            pf_ix = line.index(':')
-            platformat = line[pf_ix + 1:]
-            platformatlist_list.append(platformat)
-
-    if not platformatlist_list:
-        print("No valid formats in file 'flist.txt'")
-    else:
-        valid_format = False
-        ##pulls first video codec
-        ##not sure if we should validate all video streams or not
-        current_format = get_video_codecs(media_file)[0]
-        for pf in platformatlist_list:
-            if pf == current_format:
-                valid_format = True
-
-        if valid_format == True:
-            print(f"File is already of a valid format: {current_format}")
+        if not platform in format_dict:
+            raise Exception(f"No valid formats for {platform} in file 'flist.txt'")
         else:
-            target_format = platformatlist_list[0]
-            output_file = f"converted_{media_file}"
+            target_format = format_dict[platform]
+            current_format = get_video_codecs(media_file)[0]
 
-            print(f"File is currently of format: {current_format}")
-            print(f"Converting file to: {target_format}")
+            if target_format == current_format:
+                print(f"File is already of a valid format: {current_format}")
+            else:
+                output_file = f"converted_{media_file}"
 
-            try:
+                print(f"File is currently of format: {current_format}")
+                print(f"Converting file to: {target_format}")
+
                 target_library = get_library(target_format)
 
-                if target_library == 'missing':
+                if not target_library:
                     raise Exception(f'Codec library for {target_format} missing from clist.txt')
 
                 (
@@ -84,8 +71,8 @@ def pp_ffmpeg(platform, media_file):
                 .output(output_file,acodec='copy',vcodec=target_library)
                 .run(capture_stdout=True, capture_stderr=True)
                 )
-            except ffmpeg.Error as e:
-                print(f"Failed to transcode file {media_file}.")
-                print(e.stderr)
 
-            print("file converted")
+                print("file converted")
+    except ffmpeg.Error as e:
+        print(f"Failed to transcode file {media_file}.")
+        print(e.stderr)
